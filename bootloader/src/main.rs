009a0923590d23f7f832a520d7f712ae8834f30a
Saint Poisson <197531958+Saint951@users.noreply.github.com>
2026-06-22 17:54:35 +0200

    Update: Ajustement des .toml. main.rs primitif / Update: Adjusted all the .toml and made a primitive main.rs

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "efiapi" fn efi_main(_image_handle: *mut core::ffi::c_void, _system_table: *mut core::ffi::c_void) -> usize {
+    0
 }
